f4637be8706c52cf8aa4943c4651cb27cdb20f65
Chase <brendanstrayer@gmail.com>
2024-08-09 22:17:40 -0600

    Create Introduction component

---
+++ src/app/INTRODUCTION_COPY.ts
@@ -0,0 +1,12 @@
+export const firstParagraph = `
+    I am a software developer from Colorado. I have a passion for all things technology-related, and
+    have prior experience primarily with web development using Typescript, React, GraphQL, and PostgreSQL.
+`;
+
+export const secondParagraph = `
+    Some of the projects I have worked on previously include:
+`
+
+export const firstListItem = `Responsive data visualizations using d3 and nivo`;
+export const secondListItem = `Product demos with temporary user accounts populated with test data`;
+export const thirdListItem = `A full stack application for tracking/monitoring your reading progress`;
---
+++ src/app/Introduction.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import styles from './Introduction.module.css';
+import * as COPY from './INTRODUCTION_COPY';
+import Image from 'next/image';
+
+
+export default function Introduction() {
+    useEffect(() => {
+        const nameElement = document.getElementById('name');
+        if (nameElement) {
+            nameElement.addEventListener('mouseover', () => {
+                const randomColor = getRandomColor();
+                nameElement.style.color = randomColor;
+            });
+        }
+    }, []);
+
+    return (
+        <div className={styles.container}>
+            <Image src="/me.jpeg" alt="Picture of me" width={100} height={100} className={styles.image__mobile} unoptimized/>
+            <div>
+                <div className={styles.title}>
+                Hi, I'm <span id="name" className={styles.name}>Chase</span>
+                </div>
+                <div className={styles.content}>{COPY.firstParagraph}</div>
+                <div className={styles.content}>{COPY.secondParagraph}</div>
+                <ul className={styles.content}>
+                    <li className={styles.listItem}>{COPY.firstListItem}</li>
+                    <li className={styles.listItem}>{COPY.secondListItem}</li>
+                    <li className={styles.listItem}>{COPY.thirdListItem}</li>
+                </ul>
+            </div>
+            <Image src="/me.jpeg" alt="Picture of me" width={100} height={100} className={styles.image__desktop} unoptimized/>
+        </div>
+    );
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
