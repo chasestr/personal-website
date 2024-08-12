b93ec1f7fa0825c6ece83ab80c51fb16bcd52138
Chase <brendanstrayer@gmail.com>
2024-08-11 20:04:42 -0600

    Create copies for project information (title, description, etc.)

---
+++ src/app/PROJECTS_COPY.ts
@@ -0,0 +1,13 @@
+export const STORYSTASH_COPY = {
+  title: "StoryStash",
+  github: "https://github.com/chasestr/book-tracker",
+  technologies: ["Typescript", "React", "GraphQL", "Docker", "PostgreSQL"],
+  description: "Full-stack web application designed for tracking your reading progress"
+};
+
+export const PORTFOLIO_COPY = {
+    title: "Portfolio",
+    github: "https://github.com/chasestr/personal-website",
+    technologies: ["Typescript", "React", "NextJS"],
+    description: "Portfolio website (the one you're on right now"
+}
--- src/app/Projects.tsx
+++ src/app/Projects.tsx
@@ -1,48 +1,15 @@
 import React from "react";
 import styles from "./Projects.module.css";
 import ProjectCard from "./ProjectCard";
+import { PORTFOLIO_COPY, STORYSTASH_COPY } from "./PROJECTS_COPY";
 
 export default function Projects() {
   return (
     <div className={styles.container}>
       <div className={styles.title}>Projects</div>
       <div className={styles.card_container}>
-        <ProjectCard
-          title="StoryStash"
-          github="https://github.com/chasestr/book-tracker"
-          technologies={[
-            "Typescript",
-            "React",
-            "GraphQL",
-            "Docker",
-            "PostgreSQL",
-          ]}
-          description="Full-stack web application designed for tracking your reading progress"
-        />
-        <ProjectCard
-          title="StoryStash"
-          github="https://github.com/chasestr/book-tracker"
-          technologies={[
-            "Typescript",
-            "React",
-            "GraphQL",
-            "Docker",
-            "PostgreSQL",
-          ]}
-          description="Full-stack web application designed for tracking your reading progress"
-        />
-        <ProjectCard
-          title="StoryStash"
-          github="https://github.com/chasestr/book-tracker"
-          technologies={[
-            "Typescript",
-            "React",
-            "GraphQL",
-            "Docker",
-            "PostgreSQL",
-          ]}
-          description="Full-stack web application designed for tracking your reading progress"
-        />
+        <ProjectCard {...STORYSTASH_COPY} />
+        <ProjectCard {...PORTFOLIO_COPY} />
       </div>
     </div>
   );
