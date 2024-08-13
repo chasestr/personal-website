d252bafabcd1d34be55af9513eb27293f3856f51
Chase <brendanstrayer@gmail.com>
2024-08-12 22:41:59 -0600

    Add GithubIcon to ProjectCard component and adjust card styling

--- src/app/ProjectCard.tsx
+++ src/app/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./ProjectCard.module.css";
+import GithubLogo from "./GithubIcon";
 
 interface ProjectCardProps {
     title: string;
@@ -11,9 +12,12 @@ interface ProjectCardProps {
 const ProjectCard = (props: ProjectCardProps) => {
     return (
         <div className={styles.container}>
-            <a className={styles.title} href={props.github} target="blank">
-                {props.title}
-            </a>
+            <div className={styles.title_container}>
+                <div className={styles.title}>{props.title}</div>
+                <div className={styles.github_logo}>
+                    <a href={props.github} target="_blank"><GithubLogo/></a>
+                </div>
+            </div>
             <div className={styles.description}>{props.description}</div>
             <div className={styles.technologies}>
                 {props.technologies.map((t,idx) => <div key={idx} className={styles.technology}>{t}</div>)}
